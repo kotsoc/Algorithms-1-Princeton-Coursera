8b7878c25bb8219313f4d96f60fe5e0da673abf8
Konstantinos Peratinos <kotsosgr@gmail.com>
2017-09-11 04:41:49 +0200

    Alternative sollution brute

--- Assignment III/BruteCollinearPoints.java
+++ Assignment III/BruteCollinearPoints.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BruteCollinearPoints {
-    private double[] slope;
     private ArrayList<LineSegment> segs = new ArrayList<LineSegment>();
     private ArrayList<Integer> usedIndex = new ArrayList<Integer>();
     
@@ -25,37 +24,44 @@ public class BruteCollinearPoints {
      */
     public BruteCollinearPoints(Point[] points) { // finds all line segments containing 4 points
         if (points == null) { throw new java.lang.IllegalArgumentException(); }
+        double[] slope;
+        
         for (int i=0; i < points.length; i++) {
             slope = new double[points.length-i];
             if (points[i] == null) { throw new java.lang.IllegalArgumentException(); }
+            
             for (int j = points.length-1; j > i ; j--) {
                 if (i != j && j-i != i) {
                     if (points[j-i] == null || points[j-i] == points[i]) { 
                         throw new java.lang.IllegalArgumentException(); 
                     }
-                     slope[j-i] = points[i].slopeTo(points[j-i]);
                 }
+//                     System.out.println(slope[j-i] = points[i].slopeTo(points[j-i]));
+                    slope[j-i] = points[i].slopeTo(points[j]);
             }
-            for (int k = 0; k < points.length-i; k++) {
+            for (int k = 0; k < slope.length; k++) {
                 int z = 0; // Colinear point counter
                 Point[] col_points = new Point[4];
-                for (int m = points.length-1-i; m > k; m--){
+                
+                for (int m = slope.length-1; m > k; m--){
                     if (slope[k] == slope[m] && k != m && !usedIndex.contains(m)) {
-                        col_points[z++] = points[m];
-                        usedIndex.add(m);
+                        col_points[z++] = points[m+i];
+                        usedIndex.add(m+i);
                     } 
                 }
-                if (z == 3) {
+                if (z == 2) {
                     col_points[z] = points[i];
+                    col_points[z+1] = points[k+i];
                     Arrays.sort(col_points);
+                    usedIndex.add(i);
+                    usedIndex.add(k+i);
                     segs.add(new LineSegment(col_points[0], col_points[3]));
+                    z=0;
                 }
                 else {
                     int size = usedIndex.size();
                     for (int y =0; y < z; y++)
                     {
-//                       System.out.println("Removing" + (size-1-y) + "When z is: "+z +"and y:"+ y);
-                       
                        usedIndex.remove(size-1-y);
                     }
                 }
@@ -69,6 +75,7 @@ public class BruteCollinearPoints {
     }
     
     public LineSegment[] segments() { // the line segments
+//         System.out.println("Size is" +  numberOfSegments());
         LineSegment[] sgmts = segs.toArray(new LineSegment[segs.size()]);
         return sgmts;
     }
